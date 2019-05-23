1217a89813bef5a17ba9b86ebe7ba367728b8eec
Giuseppe Redavid <giuseppe.redavid96@gmail.com>
2019-05-23 15:59:33 +0200

    seconda activity

--- app/src/main/java/com/QUeM/TreGStore/MainActivity.java
+++ app/src/main/java/com/QUeM/TreGStore/MainActivity.java
@@ -1,8 +1,10 @@
 package com.QUeM.TreGStore;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton bottoneMario;
     private TextView testo1;
     private TextView testo2;
+    private Button switchaActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +26,21 @@ public class MainActivity extends AppCompatActivity {
         bottoneMario= (ImageButton) findViewById(R.id.malio);
         testo1=(TextView) findViewById(R.id.testo_prima_bottone);
         testo2=(TextView) findViewById(R.id.testo_dopo_bottone);
+        switchaActivity= (Button) findViewById(R.id.passaActivity);
 
         bottoneMario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 testo1.setText("");
                 testo2.setText(getString(R.string.stringa_benvenuto2));
+            }
+        });
+
+        switchaActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login=new Intent(MainActivity.this, loginActivity.class);
+                startActivity(login);
             }
         });
 
---
+++ app/src/main/java/com/QUeM/TreGStore/loginActivity.java
@@ -0,0 +1,15 @@
+package com.QUeM.TreGStore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_page);
+    }
+
+
+}
