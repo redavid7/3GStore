aede2bc235b7af3bd2db41211ac45dbb1c8b14f2
Adriano Palumbo <a.palumbo61@studenti.uniba.it>
2019-05-23 12:19:55 +0200

    image pre merge

--- app/src/main/java/com/QUeM/TreGStore/MainActivity.java
+++ app/src/main/java/com/QUeM/TreGStore/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_page);
+        setContentView(R.layout.activity_main);
     }
 }
