44f3b15d9cf5bc8bcd422d6975b0fa7da81ba7df
Adriano Palumbo <a.palumbo61@studenti.uniba.it>
2019-05-23 12:53:48 +0200

    funzionamento bottone

--- app/src/main/java/com/QUeM/TreGStore/MainActivity.java
+++ app/src/main/java/com/QUeM/TreGStore/MainActivity.java
@@ -2,6 +2,10 @@ package com.QUeM.TreGStore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView button;
+        button = findViewById(R.id.Cazzeggio);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final TextView textView = (TextView) findViewById(R.id.MaranGGGstore);
+                final TextView textView2 = (TextView) findViewById(R.id.provaGuardami);
+                    textView.setVisibility(View.INVISIBLE);
+                    textView2.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
