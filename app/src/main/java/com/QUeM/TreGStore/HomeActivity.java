f2d002f0225625db023ff36208e7c87efeca51f4
Giuseppe Redavid <giuseppe.redavid96@gmail.com>
2019-07-22 12:36:06 +0200

    Revert "Commit Sbarbanetflix #10 Aggiunta AuthenticationActivity (da fixare)"
    This reverts commit 8e2e849793b4ccbf6104b2969db3070d0897ea16.

--- app/src/main/java/com/QUeM/TreGStore/HomeActivity.java
+++ app/src/main/java/com/QUeM/TreGStore/HomeActivity.java
@@ -42,8 +42,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //startActivity(new Intent(this, AuthenticationActivity.class));
-
         //--------------------INIZIO GESTIONE SETUP ACTIVITY-------------------------------
 
         //inizializza Activity
