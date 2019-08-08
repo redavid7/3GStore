c10fa71b064bf350b6c5a275277899ab31f4417b
GabrieleRocaUniba <39295955+GabrieleRocaUniba@users.noreply.github.com>
2019-08-08 12:28:53 +0200

    Commit Gabriele #32 layout fragment promozioni

--- app/src/main/java/com/QUeM/TreGStore/FragmentPromozioni.java
+++ app/src/main/java/com/QUeM/TreGStore/FragmentPromozioni.java
@@ -1,33 +1,19 @@
 package com.QUeM.TreGStore;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.support.v4.app.Fragment;
 import android.view.ViewGroup;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
-import android.widget.TextView;
 import com.QUeM.TreGStore.DatabaseClass.Prodotti;
 import com.QUeM.TreGStore.DatabaseClass.ProdottiAdapter;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-
-import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
 
 public class FragmentPromozioni extends Fragment {
 
@@ -59,45 +45,7 @@ public class FragmentPromozioni extends Fragment {
         super.onStop();
         adapter.stopListening();
     }
-
-    //Metodo per leggere tutti i prodotti nel db
-    public ArrayList<Prodotti> readAllProd(){
-        //prodotti da restituire
-        final ArrayList<Prodotti> toRet=new ArrayList<Prodotti>();
-        //creo la connessione
-        final FirebaseFirestore db = FirebaseFirestore.getInstance();
-        //cerco tutti i file nel documento prodotti
-        db.collection("prodotti").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                int c=0;
-                // per ogni elemento in task prendo l'oggetto di tipo prodotti e lo aggiundo ai valori da ritornare
-                if (task.isSuccessful()) {
-                    for(DocumentSnapshot document : task.getResult()){
-                        toRet.add(document.toObject(Prodotti.class));
-                        Log.d(TAG, "Prodotto "+toRet.get(c).getNome());
-                        c++;
-                    }
-                }
-                else{
-                    Log.d(TAG, "Error getting documents: ", task.getException());
-                }
-            }
-        });
-        return toRet;
-    }
-
-    public boolean isDiscount(Prodotti p){
-        Calendar c = Calendar.getInstance();
-        int tmp =   p.getTempoPromozione().compareTo(c.getTime());
-        if(tmp<=0){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
+    //Set up reciclerView
     private void setUpRecyclerView(){
         Query query=prodottiRef.whereEqualTo("promozione",true);
         FirestoreRecyclerOptions<Prodotti> opzioni=new FirestoreRecyclerOptions.Builder<Prodotti>().setQuery(query, Prodotti.class).build();
--- app/src/main/java/com/QUeM/TreGStore/HomeActivity.java
+++ app/src/main/java/com/QUeM/TreGStore/HomeActivity.java
@@ -2,13 +2,21 @@ package com.QUeM.TreGStore;
 
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.media.audiofx.DynamicsProcessing;
+import android.media.tv.TvContract;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.se.omapi.Channel;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -44,7 +52,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         //--------------------------INIZIO GESTIONE UTENTE------------------------------------
         auth = FirebaseAuth.getInstance();
 
